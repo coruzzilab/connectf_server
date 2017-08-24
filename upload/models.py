7758fe3e048db58ef55820515e667d14cbbc620d
Che-Lun Juang <clj327@nyu.edu>
2017-08-23 21:43:23 -0400

    Add upload experiment endpoint

--- upload/models.py
+++ upload/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Experiment(models.Model):
+    name = models.CharField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
