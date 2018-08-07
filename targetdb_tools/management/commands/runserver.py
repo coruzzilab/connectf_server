baf8b7dd74709040f51ca9fd978f94ab27a37fdf
Zachary Juang <clj327@nyu.edu>
2018-08-07 15:24:47 -0400

    Add ignore_error in case of empty directory

--- targetdb_tools/management/commands/runserver.py
+++ targetdb_tools/management/commands/runserver.py
@@ -18,7 +18,7 @@ def clear_query_cache():
 
     for d in filterfalse(methodcaller('startswith', '.'), dirs):
         if now - datetime.datetime.fromtimestamp(os.path.getmtime(storage.path(d))) > datetime.timedelta(minutes=60):
-            shutil.rmtree(storage.path(d))
+            shutil.rmtree(storage.path(d), ignore_errors=True)
 
     for f in filterfalse(methodcaller('startswith', '.'), files):
         if now - datetime.datetime.fromtimestamp(os.path.getmtime(storage.path(f))) > datetime.timedelta(minutes=60):
