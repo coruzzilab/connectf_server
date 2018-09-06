d1e3d1f1c81c82da54b55d111a2d066b126d2a4e
Che-Lun Juang <clj327@nyu.edu>
2018-09-06 16:49:39 -0400

    Add option to delete annotation

--- querytgdb/management/commands/import_annotation.py
+++ querytgdb/management/commands/import_annotation.py
@@ -12,6 +12,9 @@ class Command(BaseCommand):
     help = "Import or update Annotations used to annotate results"
 
     def add_arguments(self, parser: ArgumentParser):
+        parser.add_argument("--delete", help="delete data from database if it doesn't appear in import",
+                            action="store_true")
+        parser.add_argument("-d", "--dry-run", help="print changes only", action="store_true")
         group = parser.add_mutually_exclusive_group(required=True)
 
         group.add_argument("-i", "--input", help="annotation file")
@@ -28,7 +31,7 @@ class Command(BaseCommand):
         elif infile:
             anno = anno.set_index('gene_id').fillna('')
 
-            in_anno = pd.read_csv(infile)
+            in_anno = pd.read_csv(infile, comment='#')
             in_anno.columns = ["gene_id", "name", "fullname", "gene_type", "gene_family"]
             in_anno = in_anno.set_index('gene_id').fillna('')
 
@@ -37,14 +40,29 @@ class Command(BaseCommand):
 
             to_update = pd.concat([
                 anno['id'],
-                in_anno.loc[changed[changed].index, :]
+                in_anno.loc[in_anno.index.isin(changed[changed].index), :]
             ], axis=1, join='inner').reset_index()
 
             new_anno = in_anno.loc[~in_anno.index.isin(anno.index), :].reset_index()
 
-            with atomic():
-                for a in (Annotation(**row._asdict()) for row in to_update.itertuples(index=False)):
-                    a.save()
+            to_delete = anno.loc[anno.index.difference(in_anno.index), :]
 
-                Annotation.objects.bulk_create(
-                    (Annotation(**row._asdict()) for row in new_anno.itertuples(index=False)))
+            if options["dry_run"]:
+                print("Update:")
+                print(to_update)
+                print("Create:")
+                print(new_anno)
+
+                if options['delete']:
+                    print("Delete:")
+                    print(to_delete)
+            else:
+                with atomic():
+                    for a in (Annotation(**row._asdict()) for row in to_update.itertuples(index=False)):
+                        a.save()
+
+                    Annotation.objects.bulk_create(
+                        (Annotation(**row._asdict()) for row in new_anno.itertuples(index=False)))
+
+                    if options['delete']:
+                        Annotation.objects.filter(pk__in=to_delete['id']).delete()
