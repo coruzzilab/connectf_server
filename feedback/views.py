8d290859c242da66253d88a3f607fd8c2d02982e
Che-Lun Juang <clj327@nyu.edu>
2019-10-21 14:50:28 -0400

    Add email to feedback

--- feedback/views.py
+++ feedback/views.py
@@ -1,10 +1,67 @@
-from django.forms.models import modelform_factory
+import logging
+
+import boto3
+import requests
+from botocore.exceptions import ClientError
+from django.conf import settings
+from django.forms import CharField
+from django.forms.models import ModelForm, ValidationError
 from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
 from django.views import View
 
 from . import models
 
-FeedbackForm = modelform_factory(models.Feedback, fields=('name', 'feedback'))
+logger = logging.getLogger(__name__)
+
+CHARSET = 'UTF-8'
+
+
+def aws_send_email(instance: models.Feedback):
+    client = boto3.client('sns',
+                          aws_access_key_id="AKIA2SRH3KC6XKMPI5M2",
+                          aws_secret_access_key="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")
+
+    try:
+        # Provide the contents of the email.
+        response = client.publish(
+            TopicArn=settings.AWS_TOPIC_ARN,
+            Message=instance.feedback,
+            Subject=f'[ConnecTF] Feedback from: {instance.name}'
+        )
+    # Display an error if something goes wrong.
+    except ClientError as e:
+        logging.error(e.response['Error']['Message'])
+    else:
+        logging.info(f"Email sent! Message ID: {response['MessageId']}")
+
+
+class FeedbackForm(ModelForm):
+    token = CharField()
+
+    def clean_token(self):
+        token = self.cleaned_data['token']
+
+        if token and settings.RECAPTCHA_SECRET:
+            try:
+                resp = requests.post(
+                    'https://www.google.com/recaptcha/api/siteverify',
+                    {
+                        'secret': settings.RECAPTCHA_SECRET,
+                        'response': token
+                    })
+
+                data = resp.json()
+
+                if not (data['success'] and data['score'] >= 0.5):
+                    raise ValidationError('you are a bot.')
+            except requests.exceptions.RequestException as e:
+                raise ValidationError("something wrong with recaptcha validation") from e
+
+        return token
+
+    class Meta:
+        model = models.Feedback
+        fields = ('name', 'feedback')
 
 
 class FeedbackList(View):
@@ -12,7 +69,9 @@ class FeedbackList(View):
 
         form = FeedbackForm(data=request.POST)
         if form.is_valid():
+            if form.cleaned_data['token']:
+                aws_send_email(form.instance)
             form.save()
             return HttpResponse(status=201)
 
-        return HttpResponseBadRequest()
+        return HttpResponseBadRequest(form.errors)
